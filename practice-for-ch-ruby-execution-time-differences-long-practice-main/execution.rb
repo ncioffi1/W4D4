e7c72e32b75f460220c7b0071202328ae57f5036
ncioffi1 <imnickcioffi@gmail.com>
2023-11-09 14:42:19 -0500

    push

--- practice-for-ch-ruby-execution-time-differences-long-practice-main/execution.rb
+++ practice-for-ch-ruby-execution-time-differences-long-practice-main/execution.rb
@@ -50,4 +50,46 @@ def sub_sum(arr1)
     max
 end
 
+def sub_sum_better(arr1)
+    # sub_sums = []
+    largest_sum = arr1[0]
+    current_sum = arr1[0]
+
+    (1...arr1.length).each do | i1 |
+        current_sum += arr1[i1]
+        if current_sum > largest_sum
+            largest_sum = current_sum
+        end
+        if current_sum <= 0
+            current_sum = 0
+        end
+    end
+    largest_sum
+
+    # (0...arr1.length).each do | i1 |
+    #     current_sum = arr1[i1]
+    #     if current_sum > largest_sum
+    #         largest_sum = current_sum
+    #     end
+    #     # sub_sums << sum
+    #     (0...arr1.length).each do | i2 |
+    #         if i2 > i1
+    #             current_sum += arr1[i2]
+    #             if current_sum > largest_sum
+    #                 largest_sum = current_sum
+    #             end
+    #         end
+    #     end
+    # end
+    # largest_sum
+    # max = sub_sums[1]
+    # sub_sums.each do |val|
+    #     if val > max
+    #         max = val
+    #     end
+    # end
+    # max
+
+end
+
 
